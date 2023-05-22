5ff3bc20e5e64bd6adf771caa55d2a21385d01bf
David Santiago <mcdinbwakpys@Outlook.com>
2023-05-21 19:33:27 -0600

    Modificando el estilo de las tarjetas de profesores

--- profesores.php
+++ profesores.php
@@ -25,31 +25,52 @@
 			</div>
 		</div>
 		<div class="main-content">
-			<div class="profesor-card">
+			
+			<div class="letter-divider">
+				<div class="letter">A</div>
+				<div class="profesor-card">
+					<div class="prof-info"><div class="prof-t">Nombre:</div><div class="prof-attr">ARIZA VELAZQUEZ EDUARDO</div></div>
+					<div class="prof-info"><div class="prof-t">Cubículo:</div><div class="prof-attr">103 P - CCO1</div></div>
+					<div class="prof-info"><div class="prof-t">Correo:</div><div class="prof-attr">eariza@solarium.cs.buap.mx</div></div>
+				</div>
+			</div>
+
+			<div class="letter-divider">
+				<div class="letter">B</div>
+				<div class="profesor-card">
+					<div class="prof-info"><div class="prof-t">Nombre:</div><div class="prof-attr">BONFIL BARRAGAN ERIKA</div></div>
+					<div class="prof-info"><div class="prof-t">Cubículo:</div><div class="prof-attr">103 - CCO3</div></div>
+					<div class="prof-info"><div class="prof-t">Correo:</div><div class="prof-attr">erika.bonfil@cs.buap.mx</div></div>
+				</div>
+			</div>
+			
+			<div class="letter-divider">
+				<div class="letter">G</div>
+				<div class="profesor-card">
+					<div class="prof-info"><div class="prof-t">Nombre:</div><div class="prof-attr">GARCIA TAMAYO ROSA</div></div>
+					<div class="prof-info"><div class="prof-t">Cubículo:</div><div class="prof-attr">103 D - CCO1</div></div>
+					<div class="prof-info"><div class="prof-t">Correo:</div><div class="prof-attr">rgtamayo@cs.buap.mx</div></div>
+				</div>
+			</div>
+
+			<div class="letter-divider">
+				<div class="letter">J</div>
+				<div class="profesor-card">
 				<div class="prof-info"><div class="prof-t">Nombre:</div><div class="prof-attr">JUAREZ DIAZ GABRIEL</div></div>
 				<div class="prof-info"><div class="prof-t">Cubículo:</div><div class="prof-attr">106-CCO5</div></div>
 				<div class="prof-info"><div class="prof-t">Correo:</div><div class="prof-attr">gabriel.juarezga@correo.buap.mx</div></div>
 			</div>
-			<div class="profesor-card">
-				<div class="prof-info"><div class="prof-t">Nombre:</div><div class="prof-attr">ARIZA VELAZQUEZ EDUARDO</div></div>
-				<div class="prof-info"><div class="prof-t">Cubículo:</div><div class="prof-attr">103 P - CCO1</div></div>
-				<div class="prof-info"><div class="prof-t">Correo:</div><div class="prof-attr">eariza@solarium.cs.buap.mx</div></div>
 			</div>
-			<div class="profesor-card">
-				<div class="prof-info"><div class="prof-t">Nombre:</div><div class="prof-attr">BONFIL BARRAGAN ERIKA</div></div>
-				<div class="prof-info"><div class="prof-t">Cubículo:</div><div class="prof-attr">103 - CCO3</div></div>
-				<div class="prof-info"><div class="prof-t">Correo:</div><div class="prof-attr">erika.bonfil@cs.buap.mx</div></div>
-			</div>
-			<div class="profesor-card">
-				<div class="prof-info"><div class="prof-t">Nombre:</div><div class="prof-attr">GARCIA TAMAYO ROSA</div></div>
-				<div class="prof-info"><div class="prof-t">Cubículo:</div><div class="prof-attr">103 D - CCO1</div></div>
-				<div class="prof-info"><div class="prof-t">Correo:</div><div class="prof-attr">rgtamayo@cs.buap.mx</div></div>
-			</div>
-			<div class="profesor-card">
+
+			<div class="letter-divider">
+				<div class="letter">M</div>
+				<div class="profesor-card">
 				<div class="prof-info"><div class="prof-t">Nombre:</div><div class="prof-attr">MARIN DORADO GUILLERMO</div></div>
 				<div class="prof-info"><div class="prof-t">Cubículo:</div><div class="prof-attr">301 - CCO2</div></div>
 				<div class="prof-info"><div class="prof-t">Correo:</div><div class="prof-attr">guillermo.marind@correo.buap.mx</div></div>
 			</div>
+			</div>
+
 		</div>
 	</div>
 
